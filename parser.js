e6bd6aea7af16d31883adb345dbe2ede6fe84597
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-12-02 20:40:11 +0300

    refactor 3

--- parser.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const users = Array.from(document.querySelectorAll('.d-table.table-fixed'));
-    return users.map(user => {
-        return user.querySelector('.Link--secondary').innerText.trim();
-    });
-})();
